a6faf77fa1a1bad2cc12644185695d2fa32b75c4
Judah Barton <judah.barton@yahoo.com>
2019-11-01 12:04:10 -0700

    add javascript trying to fix my problem, still broken though

--- js/scripts.js
+++ js/scripts.js
@@ -8,22 +8,39 @@ var beepBoop = function(value) {
     alert("Cmon Dave, enter a different number.")
   }else {
     for (var i = 0; i <= value; i++) {
-      if (three.includes(i) === true) {
+      resultArray += i;
+    };
+    resultArray.forEach(function(a){
+      if (three.includes(resultArray[a]) === true) {
         a = ("I'm sorry, Dave. I'm afraid I can't do that. ");
         resultArray += a;
-      }else if (two.includes(i) === true) {
-        a = ("Boop");
+      }else if (two.includes(resultArray[a]) === true) {
+        a = ("Boop ");
         resultArray += a;
-      }else if (one.includes(i) === true) {
-        a = ("Beep");
+      }else if (one.includes(resultArray[a]) === true) {
+        a = ("Beep ");
         resultArray += a;
+        console.log(resultArray);
       }else {
         resultArray += i;
       }
-    };
+    });
+    // if (three.includes(resultArray[0]) === true) {
+    //   a = ("I'm sorry, Dave. I'm afraid I can't do that. ");
+    //   resultArray += a;
+    // }else if (two.includes(resultArray[0]) === true) {
+    //   a = ("Boop ");
+    //   resultArray += a;
+    // }else if (one.includes(i) === true) {
+    //   a = ("Beep ");
+    //   resultArray += a;
+    //   console.log(i);
+    // }else {
+    //   resultArray += i;
+    // }
+    console.log(resultArray);
   }
   return resultArray;
-  console.log(array);
 };
 
 $(document).ready(function(){
@@ -31,7 +48,7 @@ $(document).ready(function(){
     event.preventDefault();
     var value = $("input#number-input").val();
     $("#final-answer").text(beepBoop(value));
-    console.log(value);
+
   });
 
 });
