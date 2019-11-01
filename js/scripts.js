1d08b70cd16719ad27fdc96b52ac3d6e7e132252
Judah Barton <judah.barton@yahoo.com>
2019-11-01 10:28:51 -0700

    add javascript to work on switching out numbers with beep boop

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
-var start = 0;
+var three = ("3");
+var two = ("2");
+var one = ["1"];
 
 var beepBoop = function(value) {
   var resultArray = [];
-  for (var i = 0; i <= value; i++) {
-    resultArray += i;
-    resultArray
-  };
+  if (value < 0 || value > 100) {
+    alert("Cmon Dave, enter a different number.")
+  }else {
+    for (var i = 0; i <= value; i++) {
+      if (three.includes(i) === true) {
+        i = ("I'm sorry, Dave. I'm afraid I can't do that. ");
+        resultArray += i;
+      }else if (two.includes(i) === true) {
+        i = ("Boop");
+        resultArray += i;
+      }else {
+        resultArray += i;
+      }
+    };
+  }
   return resultArray;
   console.log(array);
 };
@@ -13,7 +26,7 @@ var beepBoop = function(value) {
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var value = parseInt($("input#number-input").val());
+    var value = $("input#number-input").val();
     $("#final-answer").text(beepBoop(value));
     console.log(value);
   });
