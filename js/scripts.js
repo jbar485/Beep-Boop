35639f23c11bba4ffd36414022450c23d472b558
Judah Barton <judah.barton@yahoo.com>
2019-11-01 15:09:19 -0700

    add html and javaScript to try and add a name input to the options

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
 var three = "3";
 var two = "2";
 var one = "1";
+var name = "";
 
 var beepBoop = function(value) {
   var resultArray = [];
   if (value < 0 || value > 100) {
-    alert("Cmon Dave, enter a different number.")
+    alert("Cmon " + name + ", enter a different number.")
   }else {
     for (var i = 0; i <= value; i++) {
       if (i.toString().includes(three)) {
-        resultArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+        resultArray.push("I'm sorry," + name + " I'm afraid I can't do that.");
       }else if (i.toString().includes(two)) {
         resultArray.push("Boop");
       }else if (i.toString().includes(one)) {
@@ -25,6 +26,7 @@ var beepBoop = function(value) {
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    var name = $("input#name-input").val();
     var value = $("input#number-input").val();
     $("#final-answer").text(beepBoop(value));
 
