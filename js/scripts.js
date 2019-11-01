a651fc0cc61d134e0237d995e251cb8adf534efb
Judah Barton <judah.barton@yahoo.com>
2019-11-01 09:55:38 -0700

    add javascript to make numbers count from 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+var start = 0;
 
+var beepBoop = function(value) {
+  var resultArray = [];
+  for (var i = 0; i <= value; i++) {
+    resultArray += i;
+    resultArray
+  };
+  return resultArray;
+  console.log(array);
+};
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
-    var value = $("input#number-input").val();
+    var value = parseInt($("input#number-input").val());
+    $("#final-answer").text(beepBoop(value));
+    console.log(value);
   });
 
 });
